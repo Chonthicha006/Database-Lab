170f4bbe364f9ae6636e8ba99d78eded5835cf1b
Chonthicha Phuangfueang <126902958+Chonthicha006@users.noreply.github.com>
2023-08-15 10:31:42 +0700

    add new

---
+++ Lab1_test.sql
@@ -0,0 +1,20 @@
+create schema shop;
+-- drop database shop;
+use shop;
+CREATE TABLE Customer(
+CustomerID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CustFirstName varchar(25),
+CustMidName varchar(10),
+CustLastName varchar(25)
+);
+desc Custumer;
+use sakila;
+desc actor;
+use shop;
+CREATE TABLE SalesOrder(
+OrderID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+OrderDate date,
+CustomerID int,
+FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+);
+show tables;
